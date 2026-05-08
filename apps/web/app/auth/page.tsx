851b37be030e11016578af440a82dc75d1bbf006
tanmay5268 <tanmay.mewati94@gmail.com>
2026-05-08 12:35:12 +0530

    prep for branch switching

--- apps/web/app/auth/page.tsx
+++
@@ -1,9 +0,0 @@
-import Form from "apps/web/components/Form";
-
-export default function AuthPage() {
-  return (
-    <div className="w-screen h-screen flex items-center justify-center bg-[#0c0a10]">
-      <Form />
-    </div>
-  );
-}
